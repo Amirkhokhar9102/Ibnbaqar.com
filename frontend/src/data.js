1203ba071a94f8fadb48fa1d93db0e71fabb8d94
Amirkhokhar9102 <63934648+Amirkhokhar9102@users.noreply.github.com>
2020-11-06 18:39:53 +0400

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+
+export const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Automatic Hand Soap Dispenser',
+            cateGory: 'Health And Safety',
+            image: '../image/pr1/pr1.jpg',
+            price: 110,
+            brand: 'Ibn-e-Baqar',
+            rating:4.1,
+            newReviews: 10,
+            description: "Best High quality products" 
+        },
+        {
+            _id: '2',
+            name: 'Biccycle Front Bag',
+            cateGory: 'Sports',
+            image: '../image/pr2/pr2.jpg',
+            price: 100,
+            brand: 'Ibn-e-Baqar',
+            rating:4.0,
+            newReviews: 9,
+            description: "Best High quality products" 
+        },
+        {
+            _id: '3',
+            name: 'Wooden Cooking Utensils',
+            cateGory: 'Bath',
+            image: '../image/pr3/pr3.jpg',
+            price: 150,
+            brand: 'Ibn-e-Baqar',
+            rating:4.2,
+            newReviews: 12,
+            description: "Best High quality products" 
+        },
+        {
+            _id: '4',
+            name: 'Dish Soap Dispenser',
+            cateGory: 'Home and Kitchen',
+            image: '../image/pr4/pr4.jpg',
+            price: 90,
+            brand: 'Ibn-e-Baqar',
+            rating:4.2,
+            newReviews: 8,
+            description: "Best High quality products" 
+        },
+        {
+            _id: '5',
+            name: 'Nike White Slim',
+            cateGory: 'Shirts',
+            image: '../image/pr5/pr5.jpg',
+            price: 140,
+            brand: 'Ibn-e-Baqar',
+            rating:4.4,
+            newReviews: 16,
+            description: "Best High quality products" 
+        },
+        {
+            _id: '6',
+            name: 'Levi,s Blue $ Gray',
+            cateGory: 'Shirts',
+            image: '../image/pr6/pr6.jpg',
+            price: 100,
+            brand: 'Ibn-e-Baqar',
+            rating:4.0,
+            newReviews: 7,
+            description: "Best High quality products" 
+        },
+    ],
+};
